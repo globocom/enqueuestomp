2c9b8928f483dca347a1750ab501552e0a035d88
Janilton Maciel <janilton.maciel@g.globo>
2020-08-18 10:28:11 -0300

    add log

---
+++ log.go
@@ -0,0 +1,15 @@
+package enqueuestomp
+
+import (
+	"fmt"
+	golog "log"
+	"os"
+)
+
+var logger = golog.New(os.Stderr, "", golog.Ldate|golog.Ltime|golog.Lmicroseconds)
+
+func Printf(identifier string, format string, v ...interface{}) {
+	message := fmt.Sprintf(format, v...)
+	// logger.Printf("[EnqueueStomp] identifier: `%s` - message: `%s`", identifier, message)
+	logger.Printf("[EnqueueStomp][%s] %s", identifier, message)
+}
